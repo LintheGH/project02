113ae518635f5b3b73593919b5b00af70f320e41
GTLONG <624605432@qq.com>
2018-08-03 09:08:56 +0800

    'gtl'

---
+++ src/components/pages/Home/Components/GoodsList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="goods-list"
+   v-for = "(items,i) in list"
+  :key="i">
+    <div class="goods-content">
+      <img src="" alt="">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'GoodsList',
+  props: {
+    list: Array
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
